9bb67aa0ad0bf8a0886ce5090842373f3b5d5d9f
yin_q <NLyq181130>
2020-04-01 22:27:28 +0800

    添加项目文件。

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    static class AddTwoNumbers
+    {
+        public static List<int> AddList(List<int> l1, List<int> l2)
+        {
+            var l3 = l1.Count >= l2.Count ? l1.AddResult(l2) : l2.AddResult(l1);
+            return l3;
+        }
+        public static List<int> AddResult(this List<int> list, List<int> l2)
+        {
+            //list.Reverse();
+            //l2.Reverse();
+            var listResult = new List<int>();
+            var tmp = 0;
+            var length2 = l2.Count;
+            var length1 = list.Count;
+            for (var i = 0; i<length2;i++)
+            {
+                var node = list[i] + l2[i] + tmp;
+                if (node > 9)
+                {
+                    tmp = node / 10;
+                    node = node % 10;
+                }
+                listResult.Add(node);
+            }
+
+            for (var i = length2; i < length1; i++)
+            {
+                var node = list[i] + tmp;
+                if (node > 9)
+                {
+                    tmp = node / 10;
+                    node = node % 10;
+                }
+                listResult.Add(node);
+            }
+            return listResult;
+        }
+
+    }
+
+}
---
+++ TwoSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class TwoSum
+    {
+        public static int[] TwoSumInts(int[] nums, int target)
+        {
+            Dictionary<int, int> numsDic = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num2 = target - nums[i];
+                if (numsDic.ContainsKey(num2) && i != numsDic[num2])
+                    return new[] { i, numsDic[num2] };
+                numsDic.Add(nums[i], i);
+            }
+
+            return null;
+        }
+    }
+}
